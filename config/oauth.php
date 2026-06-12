46f59a25c45ff3d09f5d298463dd697f2d170b36
elisatisya <elisatisya1@gmail.com>
2026-06-12 09:32:10 +0700

    Create oauth.php

---
+++ config/oauth.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'demo_mode' => false, // Ubah dari true menjadi false
+
+    'google' => [
+        'client_id' => '602583550189-c6nmlmdeof0etacf4fih2d52jhmjvd7d.apps.googleusercontent.com',
+        'client_secret' => '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',
+        'redirect_uri' => 'http://localhost/wandee/auth/google_callback',
+    ]
+];
