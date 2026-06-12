b7bd9676747e83d8410a8fcf67e82b8306482aae
elisatisya <elisatisya1@gmail.com>
2026-06-12 09:30:11 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -13,4 +13,15 @@ if(!$conn){
 
 }
 
+// Auto migrate: Tambahkan kolom oauth_provider dan oauth_uid jika belum ada
+$checkProvider = mysqli_query($conn, "SHOW COLUMNS FROM `users` LIKE 'oauth_provider'");
+if (mysqli_num_rows($checkProvider) === 0) {
+    mysqli_query($conn, "ALTER TABLE `users` ADD COLUMN `oauth_provider` VARCHAR(50) DEFAULT NULL AFTER `photo`");
+}
+
+$checkUid = mysqli_query($conn, "SHOW COLUMNS FROM `users` LIKE 'oauth_uid'");
+if (mysqli_num_rows($checkUid) === 0) {
+    mysqli_query($conn, "ALTER TABLE `users` ADD COLUMN `oauth_uid` VARCHAR(255) DEFAULT NULL AFTER `oauth_provider`");
+}
+
 ?>
