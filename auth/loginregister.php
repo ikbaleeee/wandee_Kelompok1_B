57caee85b3389b6e7f59ac3c71c3adf858a82c43
Tiwi Mustika Dewi <tiwi.md21@gmail.com>
2026-06-12 20:09:50 +0700

    Delete auth directory

--- auth/loginregister.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/database.php';
-require_once __DIR__ . '/../app/controllers/AuthController.php';
-
-$controller = new AuthController($conn);
-$controller->index();
-
