6655117ed3d6a21b238fee5ecb956da3630b6e61
Tiwi Mustika Dewi <tiwi.md21@gmail.com>
2026-06-12 10:05:06 +0700

    Update and rename payment.php to payment_detail.php

---
+++ user/payment_detail.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../app/controllers/UserController.php';
+
+$controller = new UserController($conn);
+$controller->payment_detail();
